7a6f7716bca8c2f0595e934ee69031b69d458da4
Aiden Adzich <aiden.adzich@itas.ca>
2025-01-22 13:35:58 -0800

    Initial commit

---
+++ warps.js
@@ -0,0 +1,26 @@
+export var WarpUp = "";
+export var WarpRight = "";
+export var WarpDown = "";
+export var WarpLeft = "";
+export var WarpCave = "swordcave";
+
+export var WarpUpPos = { // WU
+    x: 0, // Starting X Pos
+    y: 0  // Starting Y Pos
+};
+export var WarpRightPos = { // WR
+    x: 0, // Starting X Pos
+    y: 0  // Starting Y Pos
+};
+export var WarpDownPos = { //WD
+    x: 0, // Starting X Pos
+    y: 0  // Starting Y Pos
+};
+export var WarpLeftPos = { //WL
+    x: 0, // Starting X Pos
+    y: 0  // Starting Y Pos
+};
+export var WarpCavePos = { //WC
+    x: 0, // Starting X Pos
+    y: 0  // Starting Y Pos
+};
